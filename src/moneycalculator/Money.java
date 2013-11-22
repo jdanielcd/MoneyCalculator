f4705663191df8b24124295e5e1dbc23aa554b9d
usuario <usuario@10.22.146.205>
2013-11-22 13:10:58 +0000

    1.1

---
+++ src/moneycalculator/Money.java
@@ -0,0 +1,20 @@
+package moneycalculator;
+
+
+public class Money {
+    private double amount;
+    private Currency currency;
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public Money(double amount, Currency currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+}
