eb9b3f750ae1d83bbaec81ca5c1062890fb16666
cash <cash@192.168.1.130>
2013-12-31 11:40:17 +0000

    Consola Acabada, falta BD

--- src/moneycalculator/MoneyCalculator.java
+++ src/moneycalculator/MoneyCalculator.java
@@ -1,10 +1,15 @@
 package moneycalculator;
 
+import moneycalculator.Persistance.MockCurrencySetLoader;
+import moneycalculator.View.ConsoleExchanger;
+
 
 public class MoneyCalculator {
 
     
     public static void main(String[] args) {
-     
+        new MockCurrencySetLoader().load();
+        new ConsoleExchanger().execute();
+    
     }
 }
